800751f72eca4b4a60b5af261705e7c0d7c15c0d
KuTu <haamraah@yahoo.com>
2020-01-25 12:26:56 -0700

    socket server and client initial setup

--- server.js
+++ server.js
@@ -1,61 +1,59 @@
 // Loading evnironmental variables here
-if (process.env.NODE_ENV !== 'production') {
-	console.log('loading dev environments')
-	require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  console.log("loading dev environments");
+  require("dotenv").config();
 }
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
-const SessionStore = require('express-session-sequelize')(expressSession.Store);
-const passport = require('./utils/passport');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const SessionStore = require("express-session-sequelize")(expressSession.Store);
+const passport = require("./utils/passport");
 const db = require("./models");
-const path = require('path');
+const path = require("path");
 const app = express();
 const routes = require("./controllers");
 const PORT = process.env.PORT || 8081;
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 // const db = new Sequelize('test', 'root', 'root', {
 //     host: 'localhost',
 //     dialect: 'mysql',
 // });
 
 const sequelizeSessionStore = new SessionStore({
-    db: db.sequelize,
+  db: db.sequelize
 });
- 
 
 // ===== Middleware ====
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(
-	expressSession({
-		secret: process.env.APP_SECRET || 'this is the default passphrase',
-		store: sequelizeSessionStore,
-		resave: false,
-		saveUninitialized: false,
-	})
+  expressSession({
+    secret: process.env.APP_SECRET || "this is the default passphrase",
+    store: sequelizeSessionStore,
+    resave: false,
+    saveUninitialized: false
+  })
 );
 
 // ===== Passport ====
 app.use(passport.initialize());
 app.use(passport.session()); // will call the deserializeUser
 
-
 // ==== if its production environment!
 if (process.env.NODE_ENV === "production") {
-	console.log("Prod Mode Enabled");
-	app.use(express.static("client/build"));
+  console.log("Prod Mode Enabled");
+  app.use(express.static("client/build"));
 }
 
 // ====== Routing & Controllers =====
@@ -63,28 +61,39 @@ app.use(routes);
 
 // ====== React App ======
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // ====== Error handler ====
-app.use(function (err, req, res, next) {
-	console.log('====== ERROR =======');
-	console.error(err.stack);
-	res.status(500)
+app.use(function(err, req, res, next) {
+  console.log("====== ERROR =======");
+  console.error(err.stack);
+  res.status(500);
 });
+
+// ====== socket.io server
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
 // ====== Sync Models to Database
 let force = true;
 
 if (process.env.NODE_ENV === "development") {
-	force = true;
+  force = true;
 }
 
 // ==== Starting Server =====
 
-db.sequelize.sync({ force: force }).then(function () {
-	console.log(`Sequlize connected`);
-	app.listen(PORT, () => {
-		console.log(`App listening on PORT: ${PORT}`)
-	})
-})
+db.sequelize.sync({ force: force }).then(function() {
+  console.log(`Sequlize connected`);
+  app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
+});
+
+// ============== socket.io connection
+io.on("connection", socket => {
+  socket.on("new user connected", msg => {
+    console.log(msg);
+  });
+});
