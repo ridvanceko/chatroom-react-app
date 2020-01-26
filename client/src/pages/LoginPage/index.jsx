ed72d1780edc37394c00030511963a26781f6bd3
Ridvan Cekmez <ridvanceko@gmail.com>
2020-01-25 17:10:42 -0700

    css layouts

--- client/src/pages/LoginPage/index.jsx
+++ client/src/pages/LoginPage/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 // import googleButton from './google_signin_buttons/web/1x/btn_google_signin_dark_disabled_web.png'
 import googleButton from './google_signin_buttons/web/1x/btn_google_signin_dark_normal_web.png'
-import "./css/main.css"
+import "./css/loginMain.css"
 
 import LoginForm from "../../components/LoginForm";
 
